ff686615f9574451ff6da2b6d7e53d8ad98d2f08
NileshRaut-code <nileshraut898@gmail.com>
2024-02-10 00:48:26 +0530

    backto

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-// import path from "path";
-// import { fileURLToPath } from "url";
-// import { dirname } from "path";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
 const app = express();
 
 app.use(
@@ -15,7 +10,7 @@ app.use(
     credentials: true,
   })
 );
-// app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
