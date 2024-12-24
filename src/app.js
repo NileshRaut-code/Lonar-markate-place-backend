de640ed53ef18849c8124ec6b9d0d747ee8e39ab
Nilesh Raut <82454856+NileshRaut-code@users.noreply.github.com>
2024-12-24 21:50:15 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ app.use(
   })
 );
 
+app.get("/",(req,res)=>{
+    res.send("hellow")
+})
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
