20fd872d279ccfe946b3bd1c5d051c747687a5c2
Nilesh Raut <82454856+NileshRaut-code@users.noreply.github.com>
2024-12-24 23:13:42 +0530

    Update seller.routes.js

--- src/routes/seller.routes.js
+++ src/routes/seller.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { getShopProfile } from "../controllers/seller.controller.js";
+import {
+  EditOrder,
+  getComment,
+  getShopProfile,
+} from "../controllers/seller.controller.js";
 import { verifySeller } from "../middlewares/seller.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
@@ -8,11 +12,20 @@ import {
   oneProduct,
   deleteProduct,
   updateShopProfile,
+  createComment,
+  allProducts,
+  editComment,
+  deleteComment,
+  allOrder,
 } from "../controllers/seller.controller.js";
 import { verifyCreator } from "../middlewares/creator.middleware.js";
 import { verifyOwner } from "../middlewares/owner.middleware.js";
+import { verifyreviewCreator } from "../middlewares/reviewcreator.middleware.js";
+import multer from "multer";
 const router = Router();
+const upload = multer();
 
+router.route("/allproduct").get(allProducts);
 router.route("/shop/:username").get(getShopProfile);
 
 router.route("/product/:id").get(oneProduct);
@@ -21,12 +34,26 @@ router
   .delete(verifyJWT, verifySeller, deleteProduct);
 router
   .route("/edit/product/:productId")
-  .put(verifyJWT, verifyCreator, updateProduct);
+  .put(verifyJWT, verifyCreator, upload.any("image", 1), updateProduct);
 //verifySeller,
-router.route("/create-product").post(verifyJWT, verifySeller, createProduct);
+router
+  .route("/create-product")
+  .post(verifyJWT, verifySeller, upload.any("image", 1), createProduct);
 
 router
   .route("/shop/edit/:username")
   .put(verifyJWT, verifySeller, verifyOwner, updateShopProfile);
+router.route("/product/createcomment/:id").post(verifyJWT, createComment);
+router
+  .route("/product/editcomment/:reviewId")
+  .put(verifyJWT, verifyreviewCreator, editComment);
+router
+  .route("/product/deletecomment/:reviewId")
+  .delete(verifyJWT, verifyreviewCreator, deleteComment);
+
+router.route("/product/comment/:id").get(getComment);
+
+router.route("/order/confirm").put(verifyJWT, verifySeller, EditOrder);
+router.route("/allorder").get(verifyJWT, verifySeller, allOrder);
 
 export default router;
