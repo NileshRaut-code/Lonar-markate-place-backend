c64405deb078ed03778efe2bd96186bf68183eaf
Nilesh Raut <82454856+NileshRaut-code@users.noreply.github.com>
2024-12-24 23:10:36 +0530

    Update seller.controller.js

--- src/controllers/seller.controller.js
+++ src/controllers/seller.controller.js
@@ -4,6 +4,23 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ObjectId } from "mongodb";
+import { Review } from "../models/review.model.js";
+import {
+  uploadImageToCloudinary,
+  uploadOnCloudinary,
+} from "../utils/cloudinary.js";
+import { v2 as cloudinary } from "cloudinary";
+import { Order } from "../models/order.model.js";
+const allProducts = asyncHandler(async (req, res) => {
+  const product_data = await Product.find({}).populate({
+    path: "createdBy",
+    select: "fullName",
+  });
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, product_data, "all product fetched"));
+});
 
 const createProduct = asyncHandler(async (req, res) => {
   const createdBy = req.user._id;
@@ -11,12 +28,40 @@ const createProduct = asyncHandler(async (req, res) => {
   if ([title, productdescription].some((field) => field?.trim() === "")) {
     throw new ApiError(400, "All fields are required");
   }
+
+  // const productImageLocalPath = req.file?.path;
+
+  // if (!productImageLocalPath) {
+  //   throw new ApiError(400, "product image file is missing");
+  // }
+
+  // const productImage = await uploadOnCloudinary(productImageLocalPath);
+
+  // if (!productImage.url) {
+  //   throw new ApiError(400, "Error while uploading on Product image");
+  // }
+  console.log(req.files[0]);
+  const productImageLocalPath = req?.files[0]?.buffer;
+  let productImage;
+
+  if (!productImageLocalPath) {
+    //i changed it only for vercel becoz cant stored into local
+    // productImage = await uploadOnCloudinary(productImageLocalPath);
+    throw new ApiError(400, "product image file is missing");
+  }
+  productImage = await uploadImageToCloudinary(productImageLocalPath);
+  if (!productImage.url) {
+    throw new ApiError(400, "Error while uploading on Product image");
+  }
+  console.log(productImage?.url);
   const newProduct = await Product.create({
     title,
     productdescription,
     price,
     availableStock,
     createdBy,
+    //image: productImage.url || " ",
+    image: productImage ? productImage.url : "",
   });
 
   return res
@@ -39,7 +84,7 @@ const getShopProfile = asyncHandler(async (req, res) => {
     },
     {
       $lookup: {
-        from: "products", // The name of the collection you are looking up against
+        from: "products",
         localField: "_id",
         foreignField: "createdBy",
         as: "products",
@@ -47,8 +92,8 @@ const getShopProfile = asyncHandler(async (req, res) => {
     },
     {
       $project: {
-        password: 0, // exclude password field
-        refreshToken: 0, // exclude refreshToken field
+        password: 0,
+        refreshToken: 0,
       },
     },
   ]);
@@ -65,20 +110,35 @@ const getShopProfile = asyncHandler(async (req, res) => {
 const updateProduct = asyncHandler(async (req, res) => {
   const product_id = req.params.productId;
   const { title, productdescription, price, Avaiblestock } = req.body;
-  //   console.log(title, productdescription, price, Avaiblestock);
+  const updatingfields = {};
+  if (title) {
+    updatingfields.title = title;
+  }
+  if (productdescription) {
+    updatingfields.productdescription = productdescription;
+  }
 
-  //console.log(await Product.findOne({ _id: new ObjectId(productId) }));
+  if (price) {
+    updatingfields.price = price;
+  }
+  if (Avaiblestock) {
+    updatingfields.Avaiblestock = Avaiblestock;
+  }
+
+  if (req?.files[0]?.buffer) {
+    const imageurl = await uploadImageToCloudinary(req?.files[0]?.buffer);
+    if (!imageurl) {
+      throw new ApiError(404, "Image is not Uploaded Properly");
+    }
+    updatingfields.image = imageurl.url;
+  }
+
   const updatedProduct = await Product.findByIdAndUpdate(
     {
       _id: new ObjectId(product_id),
     },
     {
-      $set: {
-        title,
-        productdescription,
-        price,
-        Avaiblestock,
-      },
+      $set: updatingfields,
     },
     { new: true }
   ).select();
@@ -94,7 +154,12 @@ const updateProduct = asyncHandler(async (req, res) => {
 const oneProduct = asyncHandler(async (req, res) => {
   const product_id = req.params.id;
 
-  const product_data = await Product.findOne({ _id: new ObjectId(product_id) });
+  const product_data = await Product.findOne({
+    _id: new ObjectId(product_id),
+  }).populate({
+    path: "createdBy",
+    select: "_id",
+  });
   if (!product_data) {
     throw new ApiError(404, "Product Does not Exist");
   }
@@ -146,6 +211,137 @@ const updateShopProfile = asyncHandler(async (req, res) => {
     );
 });
 
+const createComment = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const comment = req.body.comment;
+  const rating = req.body.rating;
+  console.log(req.body);
+  if (!id || !comment || !rating) {
+    throw new ApiError(200, "all feilds are require");
+  }
+  const productdata = await Product.findOne({ _id: new ObjectId(id) });
+  if (!productdata) {
+    throw new ApiError(404, "No Product Found");
+  }
+
+  console.log(productdata);
+  const comment_data = await Review.create({
+    product_id: id,
+    rating: rating,
+    review_comment: comment,
+    createdBy: req.user._id,
+  });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { comment_data }, "reviews fetched"));
+});
+
+const getComment = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const productdata = await Product.findOne({ _id: new ObjectId(id) });
+  if (!productdata) {
+    throw new ApiError(404, "No Product Found");
+  }
+
+  console.log(productdata);
+  const reviewdata = await Review.find({
+    product_id: new ObjectId(id),
+  }).populate({ path: "createdBy", select: "fullName avatar" });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { reviewdata }, "reviews comment fetched"));
+});
+
+const editComment = asyncHandler(async (req, res) => {
+  const reviewId = req.params.reviewId;
+  const comment = req.body.comment;
+  await Review.findByIdAndUpdate(new ObjectId(reviewId), {
+    review_comment: comment,
+  });
+  const reviewdata = await Review.findById(new ObjectId(reviewId));
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { reviewdata }, "reviews succesfull updated"));
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  const reviewId = req.params.reviewId;
+  const reviewdata = await Review.findById(new ObjectId(reviewId));
+  if (!reviewdata) {
+    throw new ApiError(404, "review Not Found");
+  }
+
+  await Review.findByIdAndDelete(new ObjectId(reviewId));
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "reviews succesfull deleted"));
+});
+// match: { createdBy: req.user._id },
+
+const allOrder = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  const allorderdata = await Order.aggregate([
+    {
+      $lookup: {
+        from: "products", // The collection to join with `Order`. Ensure this is the correct name of your products collection
+        localField: "product_id", // The field from the orders collection
+        foreignField: "_id", // The field from the products collection to match on
+        as: "productDetails", // The array field name where the joined documents will be placed
+      },
+    },
+    {
+      $unwind: "$productDetails", // Deconstructs the `productDetails` array
+    },
+    {
+      $match: {
+        "productDetails.createdBy": userId, // Filter where `createdBy` matches the current user's ID
+      },
+    },
+    {
+      $project: {
+        product_id: 1,
+        quantity: 1,
+        price: 1,
+        ordercreatedBy: 1,
+        address: 1,
+        pincode: 1,
+        payment_mode: 1,
+        status: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        total_cost: 1,
+        productDetails: {
+          createdBy: 1,
+          title: 1,
+          image: 1,
+        },
+      },
+    },
+  ]);
+  // const allorderdata = await Order.find({}).populate({
+  //   path: "product_id",
+  //   select: "createdBy title image",
+  // });
+  res.send(allorderdata);
+});
+const EditOrder = asyncHandler(async (req, res) => {
+  const { _id, status } = req.body;
+
+  const updatedOrder = await Order.findById(new ObjectId(_id));
+  if (!updatedOrder) {
+    throw new ApiError(404, "Order not Exist");
+  }
+  await Order.findByIdAndUpdate(new ObjectId(_id), {
+    $set: { status },
+  });
+
+  const updateddata = await Order.findById(new ObjectId(_id));
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updateddata, "order succesfull updated"));
+});
+
 export {
   createProduct,
   getShopProfile,
@@ -153,4 +349,11 @@ export {
   oneProduct,
   deleteProduct,
   updateShopProfile,
+  createComment,
+  allProducts,
+  getComment,
+  editComment,
+  deleteComment,
+  EditOrder,
+  allOrder,
 };
