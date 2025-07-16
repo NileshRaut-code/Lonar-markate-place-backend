941aafaa3c2db4a299a80acb21c2e90b57813224
Nilesh Raut <nileshraut898@gmail.com>
2025-07-16 20:05:54 +0530

    minor changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import crypto from "crypto";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 import { Product } from "../models/product.model.js";
@@ -401,13 +402,14 @@ const allProductsLimitpage = asyncHandler(async (req, res) => {
 const forgotPassword = asyncHandler(async (req, res) => {
     const { email } = req.body;
 
+
     const user=await User.findOne({email});
     if(!user){
         throw new ApiError(404,"User not found");
     }
-    const resetToken=user.generateResetToken(); 
-    await user.save({validateBeforeSave:false});  
-    const ResetUrl='https://'+req.get('host')+'/reset-password';
+    const resetToken=user.generatePasswordResetToken(); 
+    await user.save();  
+    const ResetUrl='https://'+req.get('host')+'/reset-password/'+resetToken;
     
     const message=`Please click on the link to reset your password: ${ResetUrl}`;
     await sendEmail({
@@ -416,23 +418,37 @@ const forgotPassword = asyncHandler(async (req, res) => {
         message,
     })
 
-    return res.status(200).json(new ApiResponse(200,{resetToken},"Reset token generated successfully"));
+    return res.status(200).json(new ApiResponse(200,{},"Reset token generated successfully"));
     
 })
 
 const resetPassword = asyncHandler(async (req, res) => {
-    const { resetToken, password } = req.body;
-    const user=await User.findOne({resetToken,resetTokenExpiry:{$gt:Date.now()}});
-    if(!user){
-        throw new ApiError(404,"User not found");
-    }
-    user.password=password;
-    user.resetToken=undefined;
-    user.resetTokenExpiry=undefined;
-    await user.save({validateBeforeSave:false});
-    return res.status(200).json(new ApiResponse(200,{user},"Password reset successfully"));
-    
-})
+  const { resetToken } = req.params;
+  const { password } = req.body;
+
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetToken: hashedToken,
+    resetTokenExpiry: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    throw new ApiError(400, "Invalid or expired password reset token");
+  }
+
+  user.password = password;
+  user.resetToken = undefined;
+  user.resetTokenExpiry = undefined;
+  await user.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password has been reset successfully."));
+});
 
 export {
   registerUser,
